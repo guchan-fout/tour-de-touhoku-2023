80f94019a625c0c6a3e550abf5ac379a75f5e58e
guchan-fout <stevengc@gmail.com>
2023-05-16 15:31:41 +0900

    First commit

---
+++ src/app/GeoJsonUtils.ts
@@ -0,0 +1,102 @@
+import { Injectable } from '@angular/core';
+import { lineString, featureCollection } from '@turf/turf';
+import { LngLatLike } from 'mapbox-gl';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeoJsonUtils {
+
+  addLayer(map: mapboxgl.Map) {
+    fetch('https://raw.githubusercontent.com/guchan-fout/map-training-demo/main/tohoku65.geojson')
+      .then(response => response.json())
+      .then(data => {
+        const lineString = {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: data.features.reduce((accumulator: any[], feature: any) => {
+              if (feature.geometry.type === "Point") {
+                accumulator.push(feature.geometry.coordinates);
+              }
+              return accumulator;
+            }, [])
+          }
+        };
+
+        console.log(lineString);
+
+      });
+
+
+  }
+
+  static async getlineString(map: mapboxgl.Map): Promise<any> {
+    const response = await fetch('https://raw.githubusercontent.com/guchan-fout/map-training-demo/main/tohoku65.geojson');
+    const data = await response.json();
+    const lineString = {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "LineString",
+        coordinates: data.features.reduce((accumulator: any[], feature: any) => {
+          if (feature.geometry.type === "Point") {
+            accumulator.push(feature.geometry.coordinates);
+            const [lng, lat] = feature.geometry.coordinates;
+            const elevation = map.queryTerrainElevation([lng, lat]);
+
+          }
+          return accumulator;
+        }, [])
+
+
+      }
+    };
+    console.log(lineString)
+    return lineString;
+  }
+
+  static async getlineStringWithElevation(map: mapboxgl.Map): Promise<any> {
+    const response = await fetch('https://raw.githubusercontent.com/guchan-fout/map-training-demo/main/tohoku65.geojson');
+    const data = await response.json();
+
+    const coordinates = data.features.reduce((accumulator: any[], feature: any) => {
+      if (feature.geometry.type === 'Point') {
+        accumulator.push(feature.geometry.coordinates);
+      }
+      return accumulator;
+    }, []);
+
+    const elevationPromises = coordinates.map((coordinate: LngLatLike) => {
+      return map.queryTerrainElevation(coordinate);
+    });
+
+    const elevations = await Promise.all(elevationPromises);
+
+    const lineString = {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'LineString',
+        coordinates: coordinates.map((coordinate: any, index: string | number) => {
+          const elevation = elevations[Number(index)];
+          return {
+            type: 'Feature',
+            properties: {
+              elevation
+            },
+            geometry: {
+              type: 'Point',
+              coordinates: coordinate
+            }
+          };
+        })
+      }
+    };
+
+    console.log(lineString);
+    return lineString;
+  }
+}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,190 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from './environment.prod';
+
+import * as geo from './GeoJsonUtils'
+import { lineString } from '@turf/turf';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tour-touhoku';
+export class AppComponent implements OnInit {
+  private map!: mapboxgl.Map;
+
+  constructor(private renderer: Renderer2, private geojsonService: geo.GeoJsonUtils) {
+
+  }
+
+  ngOnInit() {
+    this.createMap();
+  }
+
+  ngAfterViewInit() {
+    //const select = document.getElementById('language-select');
+    //this.map.getCanvasContainer().appendChild(select);
+  }
+
+  createMap() {
+    this.map = new mapboxgl.Map({
+      accessToken: environment.mapboxAccessToken,
+      container: 'map', // container ID
+      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [141.396, 38.4844], // starting position [lng, lat]
+      zoom: 11, // starting zoom
+      attributionControl: true
+    });
+
+    this.map.on('load', () => {
+      console.log('Map loaded');
+      this.addLanguageSelector();
+      this.addTerrainButton();
+      this.addRouterButton();
+    });
+  }
+
+  addLanguageSelector() {
+    // Add language selector
+    const languageSelector = document.querySelector('#language-selector select') as HTMLSelectElement;
+    languageSelector.addEventListener('change', (event) => {
+
+      const language = (event.target as HTMLSelectElement).value;
+      console.log('language:' + language);
+      this.setMapLanguage(language);
+    });
+  }
+
+  setMapLanguage(language: string) {
+    if (language) {
+      this.map.getStyle().layers.forEach((thisLayer) => {
+        console.log(thisLayer);
+        if (thisLayer.id.indexOf('-label') > 0) {
+          console.log('change ' + thisLayer.id);
+          this.map.setLayoutProperty(thisLayer.id, 'text-field', ['get', 'name_' + language])
+        }
+      });
+    }
+  }
+
+  addTerrainButton() {
+    const button = this.renderer.createElement('button');
+    const text = this.renderer.createText('Terrain switch');
+    this.renderer.appendChild(button, text);
+    this.renderer.appendChild(document.body, button);
+    // set button position
+    this.renderer.setStyle(button, 'position', 'absolute');
+    this.renderer.setStyle(button, 'top', '10%');
+    this.renderer.setStyle(button, 'left', '10px');
+    //this.renderer.setStyle(button, 'transform', 'translate(-50%, -50%)');
+    this.renderer.setStyle(button, 'z-index', '1');
+
+    // add click event listener to button
+    this.renderer.listen(button, 'click', () => {
+      this.addTerrainLayer();
+    });
+  }
+
+  addRouterButton() {
+    const button = this.renderer.createElement('button');
+    const text = this.renderer.createText('Show router');
+    this.renderer.appendChild(button, text);
+    this.renderer.appendChild(document.body, button);
+    // set button position
+    this.renderer.setStyle(button, 'position', 'absolute');
+    this.renderer.setStyle(button, 'top', '20%');
+    this.renderer.setStyle(button, 'left', '10px');
+    //this.renderer.setStyle(button, 'transform', 'translate(-50%, -50%)');
+    this.renderer.setStyle(button, 'z-index', '1');
+
+    // add click event listener to button
+    this.renderer.listen(button, 'click', () => {
+      this.addRouter();
+    });
+  }
+
+  addTerrainLayer() {
+    if (this.map) {
+      const terrainSource = this.map.getTerrain();
+      if (terrainSource) {
+        // Remove the 'terrain' layer and 'mapbox-dem' source from the map
+        this.map.setTerrain(null);
+        this.map.removeSource("mapbox-dem")
+        console.log('Terrain layer removed!');
+      } else {
+        console.log('add Terrain!');
+        this.map.addSource('mapbox-dem', {
+          'type': 'raster-dem',
+          'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+          'tileSize': 512,
+          'maxzoom': 14
+        });
+        // add the DEM source as a terrain layer with exaggerated height
+        this.map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 2.5 });
+      }
+    }
+  }
+
+  addRouter() {
+   
+    
+    geo.GeoJsonUtils.getlineString(this.map)
+      .then(lineString => {
+        this.map.addSource('router-point', {
+          type: 'geojson',
+          // Use a URL for the value for the `data` property.
+          data: lineString
+        });
+
+        this.map.addLayer({
+          'id': 'router-layer',
+          'type': 'line',
+          'source': 'router-point',
+          'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+          },
+          'paint': {
+            'line-color': '#888',
+            'line-width': 8
+          }
+        });
+
+      })
+      .catch(error => {
+        console.error(error);
+      });
+      
+      /*
+      geo.GeoJsonUtils.getlineStringWithElevation(this.map)
+      .then(lineString => {
+        this.map.addSource('router-point', {
+          type: 'geojson',
+          // Use a URL for the value for the `data` property.
+          data: lineString
+        });
+
+        this.map.addLayer({
+          'id': 'router-layer',
+          'type': 'line',
+          'source': 'router-point',
+          'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+          },
+          'paint': {
+            'line-color': '#888',
+            'line-width': 8
+          }
+        });
+
+      })
+      .catch(error => {
+        console.error(error);
+      });
+      */
+  }
 }
+
